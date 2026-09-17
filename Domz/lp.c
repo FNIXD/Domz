c976990822ac98b7fe7881f969293dbec14bf34e
FNIXD <erkimbekov.dima@bk.ru>
2026-09-18 00:07:43 +0300

    Добавьте файлы проекта.

---
+++ Domz/lp.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main() {
+    setlocale(LC_CTYPE, "RUS");
+
+    puts("      /\\");
+    puts("     /  \\");
+    puts("    /    \\");
+    puts("   /      \\");
+    puts("  /________\\");
+    puts("  |        |");
+    puts("  |  ~~~~  |");
+    puts("  |________|");
+    puts(" /          \\");
+    puts("/____________\\");
+    puts("~~~~~~~~~~~~~~~~");
+
+    puts("");
+    puts("Нажмите Enter для выхода...");
+    getchar();
+
+    return 0;
+}
